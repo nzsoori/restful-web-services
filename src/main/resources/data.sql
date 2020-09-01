77360f3ecc3edc8d5d4efa69dd23da7489d96ce8
nzsoori <30302814+nzsoori@users.noreply.github.com>
2020-09-01 17:57:25 +1200

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 insert into user(id,surname,firstname,emailaddress)
 values(10001,'Nimmakayala','Suresh','nzsoori@yahoo.com'),
-(10002,'Immadi','Sreedevi','sreedevi_immadi@yahoo.com'),
+(10002,'Emmadi','Sreedevi','sreedevi_emmadi@yahoo.com'),
 (10003,'Chappidi','Bhargavi','bhargavi_chappidi@yahoo.com'),
 (10004,'Baliboina','Balram','balram@yahoo.com'),
 (10005,'Nimmakayala','Dheeraj','dj@yahoo.com');
